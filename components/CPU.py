76892759ca2f171b422ed340b31d3107e30416e7
Tyler <psenk168@gmail.com>
2024-09-15 23:21:13 -0500

    added STA, STX, STY

--- components/CPU.py
+++ components/CPU.py
@@ -1,12 +1,5 @@
 from components.Memory import Memory
 
-"""
-Indexed indirect addressing - 6 cycles - LDA ($20, X)
-
-Indirect indexed addressing - 5+1 cycles - LDA ($20), Y
-
-"""
-
 OP_CODES = {
     'TEST': 'X',
     # LDA
@@ -17,8 +10,36 @@ OP_CODES = {
     'LDA_ABX': 0xBD,
     'LDA_ABY': 0xB9,
     'LDA_INX': 0xA1,
-    'LDA_INY': 0xB1
+    'LDA_INY': 0xB1,
     # LDX
+    'LDX_IMM': 0xA2,
+    'LDX_ZPG': 0xA6,
+    'LDX_ZPY': 0xB6,
+    'LDX_ABS': 0xAE,
+    'LDX_ABY': 0xBE,
+    # LDY
+    'LDY_IMM': 0xA0,
+    'LDY_ZPG': 0xA4,
+    'LDY_ZPX': 0xB4,
+    'LDY_ABS': 0xAC,
+    'LDY_ABX': 0xBC,
+    # STA
+    'STA_ZPG': 0x85,
+    'STA_ZPX': 0x95,
+    'STA_ABS': 0x8D,
+    'STA_ABX': 0x9D,
+    'STA_ABY': 0x99,
+    'STA_INX': 0x81,
+    'STA_INY': 0x91,
+    # STX
+    'STX_ZPG': 0x86,
+    'STX_ZPY': 0x96,
+    'STX_ABS': 0x8E,
+    # STY
+    'STY_ZPG': 0x84,
+    'STY_ZPX': 0x94,
+    'STY_ABS': 0x8C,
+
 }
 
 """
@@ -51,14 +72,6 @@ class CPU():
         # test use
         self.taken_cycles = 0
 
-    def reset(self):
-        self.pc = 0xFFFC
-        self.sp = 0x0100
-
-        self.a, self.x, self.y = 0, 0, 0
-
-        self.c, self.z, self.i, self.d, self.b, self.v, self.n = 0, 0, 0, 0, 0, 0, 0
-
     def fetch_byte(self):
         val = self.memory.data[self.pc]
         self.pc += 1
@@ -82,134 +95,438 @@ class CPU():
         instruction = self.memory.data[address]
         return instruction
 
+    def reset(self):
+        self.pc = 0xFFFC
+        self.sp = 0x0100
+
+        self.a, self.x, self.y = 0, 0, 0
+
+        self.c, self.z, self.i, self.d, self.b, self.v, self.n = 0, 0, 0, 0, 0, 0, 0
+
     def execute(self, expected_cycles):
+
+        def consume_cycles(self, cycles):
+            nonlocal expected_cycles
+            self.taken_cycles += cycles
+            expected_cycles -= cycles
 
         # fetch instruction each cycle
         while expected_cycles > 0:
 
             ins = self.fetch_byte()
-            expected_cycles -= 1
-            self.taken_cycles += 1
+            consume_cycles(self, 1)
 
             if ins == OP_CODES['LDA_IMM']:
-                data = self.fetch_byte()
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                # load
+                self.a = self.fetch_byte()
+                consume_cycles(self, 1)
 
-                self.a = data
                 self.z = 1 if self.a == 0 else 0
                 self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_ZPG']:
-                zero_page_addr = self.fetch_byte()
-                expected_cycles -= 1
-                self.taken_cycles += 1
-                val = self.read(zero_page_addr)  # reading takes no cycles
-                self.a = val
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.a = self.read(addr)
+                consume_cycles(self, 1)
+
                 self.z = 1 if self.a == 0 else 0
                 self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_ZPX']:
                 zero_page_addr = self.fetch_byte()
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
+
                 addr = zero_page_addr + self.x
-                expected_cycles -= 1
-                self.taken_cycles += 1
-                val = self.read(addr)
-                self.a = val
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
+
+                # load
+                self.a = self.read(addr)
+                consume_cycles(self, 1)
+
                 self.z = 1 if self.a == 0 else 0
                 self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_ABS']:
                 addr = self.fetch_bytes(bytes=2)
-                expected_cycles -= 2
-                self.taken_cycles += 2
-                val = self.read(addr)
-                self.a = val
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 2)
+
+                # load
+                self.a = self.read(addr)
+                consume_cycles(self, 1)
+
                 self.z = 1 if self.a == 0 else 0
                 self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_ABX']:
                 addr = self.fetch_bytes(bytes=2)
-                expected_cycles -= 2
-                self.taken_cycles += 2
+                consume_cycles(self, 2)
 
                 og_pg = addr & 0xFF00
                 new_addr = addr + self.x
+                # load
                 self.a = self.read(new_addr)
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 new_pg = new_addr & 0xFF00
                 if og_pg != new_pg:
-                    expected_cycles -= 1
-                    self.taken_cycles += 1
+                    consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_ABY']:
                 addr = self.fetch_bytes(bytes=2)
-                expected_cycles -= 2
-                self.taken_cycles += 2
+                consume_cycles(self, 2)
 
                 og_pg = addr & 0xFF00
                 new_addr = addr + self.y
+                # load
                 self.a = self.read(new_addr)
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 new_pg = new_addr & 0xFF00
                 if og_pg != new_pg:
-                    expected_cycles -= 1
-                    self.taken_cycles += 1
+                    consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_INX']:
                 addr = self.fetch_byte()
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 addr += self.x
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 val_addr = self.fetch_bytes_from_location(loc=addr, bytes=2)
-                expected_cycles -= 2
-                self.taken_cycles += 2
+                consume_cycles(self, 2)
 
+                # load
                 self.a = self.read(val_addr)
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
 
             elif ins == OP_CODES['LDA_INY']:
                 zp_addr = self.fetch_byte()
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 addr = self.fetch_bytes_from_location(loc=zp_addr, bytes=2)
-                expected_cycles -= 2
-                self.taken_cycles += 2
+                consume_cycles(self, 2)
 
                 og_pg = addr & 0xFF00
                 new_addr = addr + self.y
+
+                # load
                 self.a = self.read(new_addr)
-                expected_cycles -= 1
-                self.taken_cycles += 1
+                consume_cycles(self, 1)
 
                 new_pg = new_addr & 0xFF00
                 if og_pg != new_pg:
-                    expected_cycles -= 1
-                    self.taken_cycles += 1
+                    consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDX_IMM']:
+                # load
+                self.x = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDX_ZPG']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.x = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDX_ZPY']:
+                zero_page_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = zero_page_addr + self.y
+                consume_cycles(self, 1)
+
+                # load
+                self.x = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDX_ABS']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.x = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDX_ABY']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                og_pg = addr & 0xFF00
+                new_addr = addr + self.y
+                # load
+                self.x = self.read(new_addr)
+                consume_cycles(self, 1)
+
+                new_pg = new_addr & 0xFF00
+                if og_pg != new_pg:
+                    consume_cycles(self, 1)
+
+            elif ins == OP_CODES['LDY_IMM']:
+                # load
+                self.y = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDY_ZPG']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.y = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDY_ZPX']:
+                zero_page_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = zero_page_addr + self.x
+                consume_cycles(self, 1)
+
+                # load
+                self.y = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDY_ABS']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.y = self.read(addr)
+                consume_cycles(self, 1)
+
+                self.z = 1 if self.a == 0 else 0
+                self.n = 1 if (self.a & 0x80) != 0 else 0
+
+            elif ins == OP_CODES['LDY_ABX']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                og_pg = addr & 0xFF00
+                new_addr = addr + self.x
+                # load
+                self.y = self.read(new_addr)
+                consume_cycles(self, 1)
+
+                new_pg = new_addr & 0xFF00
+                if og_pg != new_pg:
+                    consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_ZPG']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_ZPX']:
+                zero_page_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = zero_page_addr + self.x
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_ABS']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.memory.data[addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_ABX']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                new_addr = addr + self.x
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[new_addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_ABY']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                new_addr = addr + self.y
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[new_addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_INX']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr += self.x
+                consume_cycles(self, 1)
+
+                val_addr = self.fetch_bytes_from_location(loc=addr, bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.memory.data[val_addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STA_INY']:
+                zp_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = self.fetch_bytes_from_location(loc=zp_addr, bytes=2)
+                consume_cycles(self, 2)
+
+                new_addr = addr + self.y
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[new_addr] = self.a
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STX_ZPG']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.x
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STX_ZPY']:
+                zero_page_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = zero_page_addr + self.y
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.x
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STX_ABS']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.memory.data[addr] = self.x
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STY_ZPG']:
+                addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.y
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STY_ZPX']:
+                zero_page_addr = self.fetch_byte()
+                consume_cycles(self, 1)
+
+                addr = zero_page_addr + self.x
+                consume_cycles(self, 1)
+
+                # load
+                self.memory.data[addr] = self.y
+                consume_cycles(self, 1)
+
+            elif ins == OP_CODES['STY_ABS']:
+                addr = self.fetch_bytes(bytes=2)
+                consume_cycles(self, 2)
+
+                # load
+                self.memory.data[addr] = self.y
+                consume_cycles(self, 1)
 
             elif ins == OP_CODES['TEST']:
                 pass
 
             else:
                 print(f'Code not recognized: {ins}')
-                raise ValueError
+                self.b = 1
 
 
+"""
+ADDRESSING MODES
+
+Immediate addressing - 2 cycles - LDA #10
+1. fetch opcode
+2. load value
+
+Zero page addressing - 3 cycles - LDA $10
+1. fetch opcode
+2. fetch address (zero page)
+3. load from address
+
+Zero page + X addressing - 4 cycles, LDA $10,X
+1. fetch opcode
+2. fetch address (zero page)
+3. add X to address
+5. load from address
+
+Absolute addressing - 4 cycles - LDA $00FF
+1. fetch opcode
+2-3. fetch address
+4. load from address
+
+Absolute + X/Y addressing - 4+1 cycles - LDA $2580,X
+1. fetch opcode
+2-3. fetch address
+4. add X/Y to address, load from address
+*5. if crossed pg
+
+Indexed indirect addressing - 6 cycles - LDA ($20, X)
+1. fetch opcode
+2. fetch address of address
+3. add X to address of address
+4-5. fetch address
+6. load from address
+
+Indirect indexed addressing - 5+1 cycles - LDA ($20), Y
+1. fetch opcode
+2. fetch address of address
+3-4. fetch address
+5. add Y to address, load from address
+*6. if crossed pg
+"""
--- test/Test_CPU.py
+++ test/Test_CPU.py
@@ -35,6 +35,15 @@ class Test_CPU(unittest.TestCase):
         self.assertEquals(self.cpu.v, 1)
         self.assertEquals(self.cpu.n, 1)
 
+    def test_break_flag(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 'break'
+
+        self.cpu.execute(1)
+
+        self.assertEquals(self.cpu.b, 1)
+
     def test_cpu_reset(self):
 
         self.pc = 0x0000
@@ -77,7 +86,7 @@ class Test_CPU(unittest.TestCase):
 
         self.assertEquals(self.cpu.a, 0x42)
 
-    def test_load_a_immediate_negative(self):
+    def test_load_a_immediate_negative_flag(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xA9
@@ -88,7 +97,7 @@ class Test_CPU(unittest.TestCase):
         self.assertEquals(self.cpu.a, 0xFB)
         self.assertEquals(self.cpu.n, 1)
 
-    def test_load_a_immediate_zero(self):
+    def test_load_a_immediate_zero_flag(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xA9
@@ -109,7 +118,7 @@ class Test_CPU(unittest.TestCase):
         self.cpu.execute(3)
         self.assertEquals(self.cpu.a, 0x42)
 
-    def test_load_a_zero_page_negative(self):
+    def test_load_a_zero_page_negative_flag(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xA5
@@ -120,7 +129,7 @@ class Test_CPU(unittest.TestCase):
         self.assertEquals(self.cpu.a, 0xFB)
         self.assertEquals(self.cpu.n, 1)
 
-    def test_load_a_zero_page_zero(self):
+    def test_load_a_zero_page_zero_flag(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xA5
@@ -205,7 +214,7 @@ class Test_CPU(unittest.TestCase):
         self.assertEquals(self.cpu.a, 0x42)
         self.assertEquals(self.cpu.taken_cycles, 5)
 
-    def test_load_a_index_indirect_x(self):
+    def test_load_a_indirect_x(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xA1
@@ -219,7 +228,7 @@ class Test_CPU(unittest.TestCase):
         self.assertEquals(self.cpu.a, 0x42)
         self.assertEquals(self.cpu.taken_cycles, 6)
 
-    def test_load_a_indirect_index_y(self):
+    def test_load_a_indirect_y(self):
         self.cpu.reset()
 
         self.cpu.memory.data[0xFFFC] = 0xB1
@@ -246,3 +255,284 @@ class Test_CPU(unittest.TestCase):
         self.cpu.execute(6)
         self.assertEquals(self.cpu.a, 0x42)
         self.assertEquals(self.cpu.taken_cycles, 6)
+
+    def test_load_x_immediate(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xA2
+        self.cpu.memory.data[0xFFFD] = 0x42
+
+        self.cpu.execute(2)
+
+        self.assertEquals(self.cpu.x, 0x42)
+
+    def test_load_x_zero_page(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xA6
+        self.cpu.memory.data[0xFFFD] = 0x10
+        self.cpu.memory.data[0x10] = 0x42
+
+        self.cpu.execute(3)
+        self.assertEquals(self.cpu.x, 0x42)
+
+    def test_load_x_zero_page_y(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xB6
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.y = 0x0F
+        self.cpu.memory.data[0x008F] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.x, 0x42)
+
+    def test_load_x_absolute(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xAE
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+        self.cpu.memory.data[0x2580] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.x, 0x42)
+
+    def test_load_x_absolute_y(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xBE
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+        self.cpu.y = 0x01
+        self.cpu.memory.data[0x2581] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.x, 0x42)
+        self.assertEquals(self.cpu.taken_cycles, 4)
+
+    def test_load_x_absolute_y_page_boundary_crossed(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xBE
+        self.cpu.memory.data[0xFFFD] = 0x02
+        self.cpu.memory.data[0xFFFE] = 0x44
+        self.cpu.y = 0xFF
+        self.cpu.memory.data[0x4501] = 0x42
+
+        self.cpu.execute(5)
+        self.assertEquals(self.cpu.x, 0x42)
+        self.assertEquals(self.cpu.taken_cycles, 5)
+
+    def test_load_y_immediate(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xA0
+        self.cpu.memory.data[0xFFFD] = 0x42
+
+        self.cpu.execute(2)
+
+        self.assertEquals(self.cpu.y, 0x42)
+
+    def test_load_y_zero_page(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xA4
+        self.cpu.memory.data[0xFFFD] = 0x10
+        self.cpu.memory.data[0x10] = 0x42
+
+        self.cpu.execute(3)
+        self.assertEquals(self.cpu.y, 0x42)
+
+    def test_load_y_zero_page_x(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xB4
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.x = 0x0F
+        self.cpu.memory.data[0x008F] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.y, 0x42)
+
+    def test_load_y_absolute(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xAC
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+        self.cpu.memory.data[0x2580] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.y, 0x42)
+
+    def test_load_y_absolute_x(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xBC
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+        self.cpu.x = 0x01
+        self.cpu.memory.data[0x2581] = 0x42
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.y, 0x42)
+        self.assertEquals(self.cpu.taken_cycles, 4)
+
+    def test_load_y_absolute_x_page_boundary_crossed(self):
+        self.cpu.reset()
+
+        self.cpu.memory.data[0xFFFC] = 0xBC
+        self.cpu.memory.data[0xFFFD] = 0x02
+        self.cpu.memory.data[0xFFFE] = 0x44
+        self.cpu.x = 0xFF
+        self.cpu.memory.data[0x4501] = 0x42
+
+        self.cpu.execute(5)
+        self.assertEquals(self.cpu.y, 0x42)
+        self.assertEquals(self.cpu.taken_cycles, 5)
+
+    def test_store_a_zero_page(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x85
+        self.cpu.memory.data[0xFFFD] = 0x02
+
+        self.cpu.execute(3)
+        self.assertEquals(self.cpu.memory.data[0x02], 0x42)
+
+    def test_store_a_zero_page_x(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.x = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x95
+        self.cpu.memory.data[0xFFFD] = 0x20
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x24], 0x42)
+
+    def test_store_a_absolute(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x8D
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x2580], 0x42)
+
+    def test_store_a_absolute_x(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.x = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x9D
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x2584], 0x42)
+
+    def test_store_a_absolute_y(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.y = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x99
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x2584], 0x42)
+
+    def test_store_a_indirect_x(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.x = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x81
+        self.cpu.memory.data[0xFFFD] = 0x20
+        self.cpu.memory.data[0x24] = 0x88
+
+        self.cpu.execute(6)
+        self.assertEquals(self.cpu.memory.data[0x88], 0x42)
+
+    def test_store_a_indirect_y(self):
+        self.cpu.reset()
+
+        self.cpu.a = 0x42
+        self.cpu.y = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x91
+        self.cpu.memory.data[0xFFFD] = 0x20
+        self.cpu.memory.data[0x0020] = 0x30
+        self.cpu.memory.data[0x0021] = 0x40
+
+        self.cpu.execute(6)
+        self.assertEquals(self.cpu.memory.data[0x4034], 0x42)
+
+    def test_store_x_zero_page(self):
+        self.cpu.reset()
+
+        self.cpu.x = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x86
+        self.cpu.memory.data[0xFFFD] = 0x02
+
+        self.cpu.execute(3)
+        self.assertEquals(self.cpu.memory.data[0x02], 0x42)
+
+    def test_store_x_zero_page_y(self):
+        self.cpu.reset()
+
+        self.cpu.x = 0x42
+        self.cpu.y = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x96
+        self.cpu.memory.data[0xFFFD] = 0x20
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x24], 0x42)
+
+    def test_store_x_absolute(self):
+        self.cpu.reset()
+
+        self.cpu.x = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x8E
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x2580], 0x42)
+
+    def test_store_y_zero_page(self):
+        self.cpu.reset()
+
+        self.cpu.y = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x84
+        self.cpu.memory.data[0xFFFD] = 0x02
+
+        self.cpu.execute(3)
+        self.assertEquals(self.cpu.memory.data[0x02], 0x42)
+
+    def test_store_y_zero_page_x(self):
+        self.cpu.reset()
+
+        self.cpu.y = 0x42
+        self.cpu.x = 0x04
+        self.cpu.memory.data[0xFFFC] = 0x94
+        self.cpu.memory.data[0xFFFD] = 0x20
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x24], 0x42)
+
+    def test_store_y_absolute(self):
+        self.cpu.reset()
+
+        self.cpu.y = 0x42
+        self.cpu.memory.data[0xFFFC] = 0x8C
+        self.cpu.memory.data[0xFFFD] = 0x80
+        self.cpu.memory.data[0xFFFE] = 0x25
+
+        self.cpu.execute(4)
+        self.assertEquals(self.cpu.memory.data[0x2580], 0x42)
